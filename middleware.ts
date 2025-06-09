e337da97d9d4ac00c96374073ce904f0c96673f1
nguyendevweb123 <nguyenunderground@gmail.com>
2025-06-09 14:23:03 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value
 
-  // Nếu chưa có token và truy cập vào trang cần bảo vệ
+  // Nếu chưa đăng nhập → redirect về '/'
   if (!token) {
     return NextResponse.redirect(new URL('/', request.url))
   }
@@ -14,7 +14,7 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
+    '/product/:path*',
     '/hello',
-    '/product/:path*',   // Bảo vệ tất cả route trong /product
   ],
 }
