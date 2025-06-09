a6301cdb16963244434dafe88363251136e664df
nguyendevweb123 <nguyenunderground@gmail.com>
2025-06-09 10:07:05 +0700

    f

--- app/Components/services/productService.ts
+++ app/Components/services/productService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/products';
+const API_URL = 'https://fe-psi-nine.vercel.app/';
 
 export const getProducts = async () => {
   const res = await axios.get(API_URL);
